e57fbe985d12255734685f27f9e3ab5778527277
Jesper Dreisig <dreisig90@gmail.com>
2017-12-21 09:55:34 +0100

    Initial commit on new theme

--- config.js
+++ config.js
@@ -1,3 +1,21 @@
 module.exports = {
-    category_ticket_field: process.env.CATEGORY_TICKET_FIELD
+    category_ticket_field: process.env.CATEGORY_TICKET_FIELD,
+    categories: [
+        {
+            id: 115000739129,
+            icon: 'cogwheel.svg'
+        },
+        {
+            id: 200903769,
+            icon: 'face.svg'
+        },
+        {
+            id: 200903779,
+            icon: 'heart.svg'
+        },
+        {
+            id: 200929635,
+            icon: 'puzzle.svg'
+        }
+    ]
 }
